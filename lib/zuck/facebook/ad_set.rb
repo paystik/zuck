06d2d714c3382f06335646ab6e0c20b9e673f7f8
Keith Woody <keith.woody@gmail.com>
2015-10-23 18:48:09 -0700

    l/z/f/ad_set
    * fix typo
    * add more comments based on API reference

--- lib/zuck/facebook/ad_set.rb
+++ lib/zuck/facebook/ad_set.rb
@@ -6,53 +6,51 @@ module Zuck
     # An ad set is a group of ads.
     # Known keys as per
     # [fb docs](https://developers.facebook.com/docs/marketing-api/reference/ad-campaign/)
-    known_keys :adlables,
-               :adset_schedule,
+    known_keys :adlabels,
+               :adset_schedule,    #Ad set schedule, representing a delivery schedule for a single day
                :id,
-               :account_id,
-               :bid_amount,
-               :bid_info,
+               :account_id,        #Ad Account ID
+               :bid_amount,        #Bid amount for this ad set, defined as your true value bid based on optimization_goal. This field is not available if is_autobid is true or when bid_info is returned.
+               :bid_info,          #Map of bid objective to bid value. This field is not available if is_autobid is true.
                :billing_event,     #Possible values are APP_INSTALLS, CLICKS, IMPRESSIONS, LINK_CLICKS, OFFER_CLAIMS, PAGE_LIKES, POST_ENGAGEMENT, VIDEO_VIEWS.
-               # :campaign,          #The campaign that contains this ad set
-               :campaign_id,
+               :campaign_id,       #parent campaign that contains this ad set
                :configured_status, #Possible values are ACTIVE, PAUSED, ARCHIVED, DELETED.
                :created_time,
                :creative_sequence, #Order of the adgroup sequence to be shown to users
                :effective_status,  #Possible values are ACTIVE, PAUSED, ARCHIVED, DELETED, CAMPAIGN_PAUSED.
-               :end_time,
+               :end_time,          #End time, in UTC UNIX timestamp
                :frequency_cap,
                :frequency_cap_reset_period,
                :is_autobid,        #Did the advertiser express the intent to bid automatically. This field is not available if bid_info or bid_amount is returned.
                :lifetime_fequency_cap,
-               :lifetime_imps,
+               :lifetime_imps,     #Lifetime impressions. Available only for campaigns with buying_type=FIXED_CPM
                :name,
                :optimization_goal, #NONE, APP_INSTALLS, BRAND_AWARENESS, CLICKS, ENGAGED_USERS, EXTERNAL, EVENT_RESPONSES, IMPRESSIONS, LINK_CLICKS, OFFER_CLAIMS, OFFSITE_CONVERSIONS, PAGE_ENGAGEMENT, PAGE_LIKES, POST_ENGAGEMENT, REACH, SOCIAL_IMPRESSIONS, VIDEO_VIEWS
-               :product_ad_behavior,
-               :promoted_object,
-               :rf_prediction_id,
-               :rtb_flag,
+               :product_ad_behavior, #Option to choose between prospecting and retargeting
+               :promoted_object,   #The object this ad set is promoting across all its ads.
+               :rf_prediction_id,  #Reach and frequency prediction ID
+               :rtb_flag,          #Whether this adset is using RTB or not
                :start_time,
                :targeting,
                :updated_time,
                :use_new_app_click, #If set, allows Mobile App Engagement ads to optimize for LINK_CLICKS
-               :pacing_type,
-               :budget_remaining,
-               :daily_budget,
-               :lifetime_budget
+               :pacing_type,       #Defines the pacing type, standard or using ad scheduling
+               :budget_remaining,  #Remaining budget
+               :daily_budget,      #The daily budget of the set defined in your account currency.
+               :lifetime_budget    #The lifetime budget of the set defined in your account currency.
 
     parent_object :campaign, as: :campaign_id
     list_path     :adsets
-    connections   :ad_groups, :ad_creatives, :insights
+    connections   :ad_groups,     #The ads under this ad set
+                  :ad_creatives,  #Reference document for using Facebook Marketing APIs to manage your ad creative
+                  :insights       #Ad Insights API  feat. eduping across child objects, sorting, async reports.
     #TODO: add connections where appropriate for
-    # activitities,    NEEDS research
-    # adcreatives,     EXISTS as :ad_creatives
-    # ads,             EXISTS as :ad_groups
-    # asyncadrequests, NEEDS research
-    # reachestimate,   NEEDS research
-    # targeting_sentencelines, NEEDS research
-    # insights,        EXISTS
-    # conversions,     NEEDS research
-    # stats            NEEDS research
+    # activitities,            #Facebook Ads Activity Log API
+    # asyncadrequests,         #Async ad requests for this ad set
+    # reachestimate,           #Query for the reach estimate for a specific targeting or ad
+    # targeting_sentencelines, #Object representing the human-readable description of a targeting spec
+    # conversions,             #Reference document for using Facebook Marketing APIs to get conversion stats on your Facebook ads, ad sets, and accounts.
+    # stats                    #Reference document for using Facebook Marketing APIs to get ad statistics on impressions, clicks, performance, and more
 
     #look what I learned over in ad_group
     def self.create(graph, data, campaign)
